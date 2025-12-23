27ec4873e3b872cef96640e645222a55eb5bd8a6
Claude Joseph Manbodh Jr. 'Joe <63468241+cjmDevelop@users.noreply.github.com>
2025-12-23 15:04:54 -0500

    Added cascade delete to User entity for related records

--- src/main/java/com/authservice/model/User.java
+++ src/main/java/com/authservice/model/User.java
@@ -1,6 +1,7 @@
 package com.authservice.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,6 +17,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -91,6 +94,23 @@ public class User implements UserDetails{
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    // Cascade delete relationships - when user is deleted, related records are also deleted
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<PasswordResetToken> passwordResetTokens = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<VerificationToken> verificationTokens = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<Note> notes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<QuizResult> quizResults = new ArrayList<>();
 
     @PrePersist
     protected void onCreate() {
